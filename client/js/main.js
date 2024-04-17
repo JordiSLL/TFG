c83e14ad8e9cc85cbcb1a2f8944d1be9bbe132ae
Jordi Sanchez <jsanchezlloansi@gmail.com>
2024-04-17 22:25:12 +0200

    Buscaador usuari

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
     enumerateDevices();
 });
+const data = [{
+    "_id": "661fde3fb9094d628bb7c14b",
+    "name": "Nero02",
+    "date": "2024-04-18",
+    "code": "CB696688695FR"
+},
+{
+    "_id": "661fdf95cd2ea3cda8b6af3d",
+    "name": "Jordi",
+    "date": "0001-01-01",
+    "code": "CB696688695FR"
+},
+{
+    "_id": "661fe70ee3b0ad286dd8e0d8",
+    "name": "Jordi",
+    "date": "2024-04-12",
+    "code": "CB696688695FR"
+},
+{
+    "_id": "661fe714e3b0ad286dd8e0d9",
+    "name": "Nero02",
+    "date": "2024-04-07",
+    "code": "990618"
+},
+{
+    "_id": "661fe71ce3b0ad286dd8e0da",
+    "name": "NeroBot",
+    "date": "2024-04-04",
+    "code": "VYNTWBD"
+},
+{
+    "_id": "661fe747e3b0ad286dd8e0db",
+    "name": "Josep Sanchez Aldeguer",
+    "date": "2024-04-19",
+    "code": "1234"
+},
+{
+    "_id": "661fe757e3b0ad286dd8e0dc",
+    "name": "Jordi Sanchez Lloansi",
+    "date": "2024-04-20",
+    "code": "123456789"
+}];
 const videoSelect = document.getElementById("videoSource");
 const audioSelect = document.getElementById("audioSource");
 
@@ -18,7 +60,8 @@ const newUcloseBtn = document.getElementById("newU-close-btn");
 const newPacientForm = document.getElementById("createUserForm");
 const createNewUser = document.getElementById("newU-save-btn");
 
-const searchUser = document.getElementById("searchUser");
+const searchInput = document.getElementById('searchInput');
+const searchResults = document.getElementById('searchResults');
 
 profileBtn.addEventListener("click", toggleDropdown);
 settingsBtn.addEventListener("click", toggleConfiguration);
@@ -27,7 +70,6 @@ confCloseBtn.addEventListener("click", closeConfiguration);
 newUcloseBtn.addEventListener("click", closeNewUser);
 saveBtn.addEventListener("click", saveConfiguration);
 newUsaveBtn.addEventListener("click", openNewUser);
-searchUser.addEventListener("click", searchInfoUser);
 
 function toggleDropdown(e) {
     e.stopPropagation();
@@ -45,16 +87,16 @@ function toggleConfiguration(e) {
 function logout(e) {
     e.stopPropagation();
     fetch('/api/users/logout')
-      .then(response => {
-        if (response.ok) {
-          location.assign('/');
-        } else {
-          console.error('Error en el logout:', response.status);
-        }
-      })
-      .catch(error => {
-        console.error('Error al intentar hacer logout:', error);
-      });
+        .then(response => {
+            if (response.ok) {
+                location.assign('/');
+            } else {
+                console.error('Error en el logout:', response.status);
+            }
+        })
+        .catch(error => {
+            console.error('Error al intentar hacer logout:', error);
+        });
 }
 
 function closeConfiguration(e) {
@@ -83,49 +125,49 @@ function searchInfoUser(e) {
     e.stopPropagation();
     console.log("bUSCAR");
     fetch('/api/pacient/')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
-    });
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
 }
 
-createNewUser.addEventListener('click', async (event)=> {
+createNewUser.addEventListener('click', async (event) => {
     event.preventDefault();
     const msjNewUserForm = document.getElementById("msj");
-    msjNewUserForm.textContent ="";
+    msjNewUserForm.textContent = "";
     const PacientData = {
         name: newPacientForm.name.value,
         date: newPacientForm.date.value,
         code: newPacientForm.code.value
-      };
+    };
 
-console.log(PacientData);
+    console.log(PacientData);
 
-try {
-    const res = await fetch('/api/pacient/create', { 
-      method: 'POST', 
-      body: JSON.stringify(PacientData),
-      headers: {'Content-Type': 'application/json'}  
-    });
-    console.log("HERE");
-    if (res.ok) {
-        newUser.classList.remove("active");
-        msjNewUserForm.classList.remove("error");
-    } else if (res.status === 400) {
-      const msj = await res.json();
-      msjNewUserForm.textContent = msj.message;
-      msjNewUserForm.classList.add("error");
-    } else {
-      throw new Error('Error de red o código de estado no esperado');
+    try {
+        const res = await fetch('/api/pacient/create', {
+            method: 'POST',
+            body: JSON.stringify(PacientData),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        console.log("HERE");
+        if (res.ok) {
+            newUser.classList.remove("active");
+            msjNewUserForm.classList.remove("error");
+        } else if (res.status === 400) {
+            const msj = await res.json();
+            msjNewUserForm.textContent = msj.message;
+            msjNewUserForm.classList.add("error");
+        } else {
+            throw new Error('Error de red o código de estado no esperado');
+        }
+    } catch (error) {
+        console.error('Error al registrar el usuari:', error);
     }
-  } catch (error) {
-    console.error('Error al registrar el usuari:', error);
-  }
 });
 
 // Tanquem les pop ups quan clickem fora
@@ -138,6 +180,9 @@ document.addEventListener("click", function (e) {
     }
     if (!e.target.closest(".popup")) {
         newUser.classList.remove("active");
+    }
+    if (e.target !== searchInput && e.target !== searchResults) {
+        searchResults.innerHTML = '';
     }
 });
 
@@ -179,12 +224,51 @@ function saveSettings() {
     localStorage.setItem("selectedAudioDevice", audioSelect.value);
 }
 
-document.querySelector('input[type="date"]').addEventListener('input', function() {
+document.querySelector('input[type="date"]').addEventListener('input', function () {
     if (this.value) {
-       this.classList.add('date-input--has-value');
+        this.classList.add('date-input--has-value');
     } else {
-       this.classList.remove('date-input--has-value');
+        this.classList.remove('date-input--has-value');
     }
-   });
+});
+
+
+
+function filterResults(query) {
+    return data.filter(item => {
+        const name = item.name.toLowerCase();
+        const code = item.code.toLowerCase();
+        const lowerQuery = query.toLowerCase();
+        return name.includes(lowerQuery) || code.includes(lowerQuery);
+    });
+}
+
+function showResults(results) {
+    searchResults.innerHTML = '';
+    results.forEach(item => {
+        const option = document.createElement('div');
+        option.textContent = item.name;
+        option.classList.add('search-result');
+        option.addEventListener('click', () => selectUser(item._id, item.name));
+        searchResults.appendChild(option);
+    });
+}
+
+function selectUser(userId, userName) {
+    sessionStorage.setItem('selectedUserId', userId);
+    searchInput.value = userName;
+    searchResults.innerHTML = '';
+}
+
+searchInput.addEventListener('input', () => {
+    const query = searchInput.value.trim();
+    const filteredResults = filterResults(query);
+    showResults(filteredResults);
+});
+
+searchInput.addEventListener('focus', () => {
+    showResults(data);
+});
+
 
 
